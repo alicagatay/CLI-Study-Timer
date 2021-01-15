23399f068e3bd026e881f563a40a233e0beedb26
Ali Cagatay <aliccagatay@gmail.com>
2021-01-15 23:37:27 +0300

    Changed the format of the code.

--- CLI_Study_Timer.py
+++ CLI_Study_Timer.py
@@ -9,17 +9,17 @@ def notify(title, message):
     os.system('terminal-notifier {}'.format(' '.join([m, t])))
 
 
-#This is a method for the pomodoro studying method. A pomodoro consists of 25 minutes work and 5 minutes break.
+# This is a method for the pomodoro studying method. A pomodoro consists of 25 minutes work and 5 minutes break.
 def pomodoro():
-    #t stands for the total time required for 1 pomodoro, which is 30 minutes.(25 minutes of work + 5 minutes of break.)
+    # t stands for the total time required for 1 pomodoro, which is 30 minutes.(25 minutes of work + 5 minutes of break.)
     t = 30
-    #count stands for the number of sessions that we want use the pomodoro method.
+    # count stands for the number of sessions that we want use the pomodoro method.
     count = int(input("How many pomodoro sessions do you wanna do: "))
 
     while(count > 0):
         print("Work for 25 minutes.")
-        #Using the notify method we created at the start.
-        notify(title = 'Session Change', message  = 'Get into work!')
+        # Using the notify method we created at the start.
+        notify(title='Session Change', message='Get into work!')
         print()
         print()
         while(t > -1):
@@ -28,64 +28,53 @@ def pomodoro():
 
             if(t == 5):
                 print("Take a break for 5 minutes.")
-                #Using the notify method we created at the start.
-                notify(title = 'Session Change', message  = 'Take a break')
+                # Using the notify method we created at the start.
+                notify(title='Session Change', message='Take a break')
                 print()
                 print()
 
         count = count - 1
         t = 30
     print("Pomodoro session finished.")
-    #Using the notify method we created at the start.
-    notify(title = 'Work session finished', message  = 'Congratulations!')
+    # Using the notify method we created at the start.
+    notify(title='Work session finished', message='Congratulations!')
 
 
-
-
-
-
-
-
-#5217 method consists of working for 52 minutes and having a break for 17 minutes.
+# 5217 method consists of working for 52 minutes and having a break for 17 minutes.
 def fiftytwoseventeen():
-    #t stands for the total time required for 1 pomodoro, which is 30 minutes.(25 minutes of work + 5 minutes of break.)
+    # t stands for the total time required for 1 pomodoro, which is 30 minutes.(25 minutes of work + 5 minutes of break.)
     t = 69
-    #c stands for the number of sessions that we want use the 5217 method.
+    # c stands for the number of sessions that we want use the 5217 method.
     c = int(input("How many sessions do you want to use 5217 method with: "))
 
     while(c > 0):
         print("Work for 52 minutes")
-        #Using the notify method we created at the start.
-        notify(title = 'Session Change', message  = 'Get into work!')
+        # Using the notify method we created at the start.
+        notify(title='Session Change', message='Get into work!')
         print()
         print()
         while(t > -1):
             t = t - 1
             time.sleep(60)
 
-
             if(t == 17):
                 print("Take a break for 17 minutes.")
-                #Using the notify method we created at the start.
-                notify(title = 'Session Change', message  = 'Take a break')
+                # Using the notify method we created at the start.
+                notify(title='Session Change', message='Take a break')
                 print()
                 print()
-
 
         c = c - 1
         t = 69
     print("5217 session finished.")
-    #Using the notify method we created at the start.
-    notify(title = 'Work session finished', message  = 'Congratulations!')
+    # Using the notify method we created at the start.
+    notify(title='Work session finished', message='Congratulations!')
 
 
-
-
-
-#Asking to the user which method they want to use to help them study, pomodoro or 5217.
+# Asking to the user which method they want to use to help them study, pomodoro or 5217.
 choice = input("Which study timer do you want to use, pomodoro or 5217? ")
 
-#There are 2 possible answers. The user will either choose pomodoro or 5217 method. Any other answers will cause an error and the app will close itself.
+# There are 2 possible answers. The user will either choose pomodoro or 5217 method. Any other answers will cause an error and the app will close itself.
 if(choice == "pomodoro"):
     pomodoro()
 elif(choice == "5217"):
